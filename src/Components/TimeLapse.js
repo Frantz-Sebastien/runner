8d453b1086721200c5f49ff8226cf106f2a8fa53
Frantz-Sebastien Mathias <frantz-sebastienmathias@pursuit.org>
2024-06-20 20:07:51 -0400

    fixing the TimeLapse

---
+++ src/Components/TimeLapse.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+const display = document.getElementById("display")
+
+let startTime = 0;
+let elapsedtime = 0;
+let isRunning = false;
+
+function TimeLapse(){
+    const [time, setTime] = useState(0)
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+module.exports
